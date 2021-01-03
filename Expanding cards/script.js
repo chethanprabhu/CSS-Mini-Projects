bbe5ca38debecebe421e37a213b722ac6a77e233
Chethan Prabhu <chethanprabhu65@gmail.com>
2021-01-03 07:13:57 +0530

    finished

--- Expanding cards/script.js
+++ Expanding cards/script.js
@@ -1,1 +1,14 @@
-//script
+let panels = document.querySelectorAll(".panel");
+
+panels.forEach(panel => {
+    panel.addEventListener("click", () => {
+        removeAllActiveClasses();
+        panel.classList.add("active");
+    })
+})
+
+const removeAllActiveClasses = () => {
+    panels.forEach(panel => {
+        panel.classList.remove("active");
+    })
+}
